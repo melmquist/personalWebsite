812b1b94cdcd7a549ca18ec63eeeac0c4ea7a964
Matthew Elmquist <mattelmquist@gmail.com>
2017-03-11 17:46:48 -0500

    last section animation

--- js/justScroll.js
+++ js/justScroll.js
@@ -1,6 +1,19 @@
+//animate.css pop up around divs containing links and all text/images
+//for each page.  Have them fade in on arrival into section
+
+//Last minute pull back of hidden shade to simulate on scroll snap hold
+//soccer passes you wanted to do
+
+//HANDWRITING POSTCARD AS LAST ANIMATION SCENE PICTURESQUE
+
+
 $( document ).ready(function() {
 
+
+
   var height = $(window).height()
+  var width = $(window).width();
+
   $( window ).resize(function() {
     height = $(window).height()
   });
@@ -23,6 +36,33 @@ $( document ).ready(function() {
   var interThreeEnd = height * 4.5
 
   var myLinksStart = height * 4.1
+
+
+
+//start them all off  screen
+
+  var mtnWaterFront = $('#mtnWaterFront');
+  var mtnGround = $('#mtnGround');
+  var mtnMtn = $('#mtnMtn');
+  var mtnClouds = $('#mtnClouds');
+  var mtnMoon = $('#mtnMoon');
+
+  //bottom, right, left, top initially positioned png later to be tweened in
+  mtnWaterFront.css('top', height + "px");
+  mtnGround.css('left', width + "px");
+  mtnMtn.css('left', -width + "px");
+  mtnClouds.css('top', -height + "px");
+
+
+
+
+
+
+
+
+
+
+
 
 
   $(window).scroll(function() {
@@ -69,20 +109,21 @@ $( document ).ready(function() {
       var dopeSauce = $('#dopeSauce');
       var dopeSauceCursor = $('#dopeSauceCursor');
 
-      TweenMax.to(deskPng, 1, {left: '0px', top: '0px'})
-      TweenMax.to(chairPng, 1.5, {left: '205px', top: '140px'})
-      TweenMax.to(bookPng, 1.1, {left: '145px'})
-      TweenMax.to(treePng, 1.3, {left: '50px'})
-      TweenMax.to(puterPng, 1.6, {top: '0px'})
+      TweenMax.to(deskPng, 1, {left: '-45px', top: '0px'})
+      TweenMax.to(chairPng, 1.5, {left: '195px', top: '175px'})
+      TweenMax.to(bookPng, 1.1, {left: '135px', top: '165px'})
+      TweenMax.to(puterPng, 1.6, {left: '120px', top: '8px'})
+      TweenMax.to(treePng, 1.3, {left: '15px'})
 
-      //CHANGE COLOR ITS TOO HARD TO LOOK AT FUCK...
-      // setTimeout(() =>{
-      //   dopeSauce.css("visibility", "visible")
-      //   dopeSauce.addClass("animated bounceIn");
-      //
-      //   dopeSauceCursor.css("visibility", "visible")
-      //   dopeSauceCursor.addClass("animated flash infinite");
-      // }, 2000);
+      //DOPE SAUCE
+      // CHANGE COLOR ITS TOO HARD TO LOOK AT FUCK...
+      setTimeout(() =>{
+        dopeSauce.css("visibility", "visible")
+        dopeSauce.addClass("animated bounceIn");
+
+        dopeSauceCursor.css("visibility", "visible")
+        dopeSauceCursor.addClass("animated flash infinite");
+      }, 3000);
 
     }
 
@@ -127,31 +168,38 @@ $( document ).ready(function() {
 
 
     //myLinks
-    if (pos > myLinksStart){
+    if (pos > myLinksStart + 200){
       console.log("myLinks");
+
+
+      TweenMax.to(mtnWaterFront, 1.6, {left: '0px', top: '0px'})
+      TweenMax.to(mtnGround, 2.1, {left: '0px', top: '0px'})
+      TweenMax.to(mtnMtn, 1, {left: '0px', top: '0px'})
+      TweenMax.to(mtnClouds, 1.3, {left: '0px', top: '0px'})
+
+
     }
 
   })
 
-  var screenWidth = $(window).width();
-  // console.log("screenWidth: ", screenWidth);
+  // console.log("width: ", width);
   // console.log("SC HEIGHT", height);
 
   var slides = $(".slide");
   var currentSlide = 0;
 
-  TweenMax.set(slides.filter(":gt(0)"), {left: "-" + screenWidth + "px"});
+  TweenMax.set(slides.filter(":gt(0)"), {left: "-" + width + "px"});
   TweenMax.delayedCall(4, nextSlide);
 
   function nextSlide(){
-    TweenMax.to(slides.eq(currentSlide), 1, {left: "-" + screenWidth + "px"})
+    TweenMax.to(slides.eq(currentSlide), 1, {left: "-" + width + "px"})
     if (currentSlide < slides.length - 1) {
       currentSlide++;
     } else {
       currentSlide = 0;
     }
 
-    TweenMax.fromTo(slides.eq(currentSlide), 1, {left: screenWidth + "px"}, {left: "0px"} );
+    TweenMax.fromTo(slides.eq(currentSlide), 1, {left: width + "px"}, {left: "0px"} );
     TweenMax.delayedCall(4, nextSlide)
   }
 
